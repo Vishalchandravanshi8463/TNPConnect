23d5467f0b6608f494f95cb8dc3db11ee7d9c1f4
Your Name <you@example.com>
2024-03-09 01:59:00 +0530

    Change in SignUpWithNumber Activity

--- app/src/main/java/com/example/tnpconnect/SignUpActivity.kt
+++ app/src/main/java/com/example/tnpconnect/SignUpActivity.kt
@@ -4,10 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class SignUpActivity : AppCompatActivity() {
 
-    lateinit var btnSignUp:Button
+    lateinit var btnSignUp:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
