8518ac000af33eab898d2da107d9416f983d9f17
Shivam Kumar Jha <shkrjh@gmail.com>
2024-03-09 16:12:40 +0530

    no changes

--- app/src/main/java/com/example/tnpconnect/SignUpActivity.kt
+++ app/src/main/java/com/example/tnpconnect/SignUpActivity.kt
@@ -36,9 +36,5 @@ class SignUpActivity : AppCompatActivity() {
         btnSignInn.setOnClickListener {
             startActivity(Intent(this,SignIn::class.java))
         }
-
-        fun show() {
-
-        }
     }
 }
