8446b6b2ffbd21e03d0d8257d1adedf70202447e
Your Name <you@example.com>
2024-03-23 06:39:36 +0530

    Navigation bar completed

--- app/src/main/java/com/test/tnpconnect/MainActivity.kt
+++ app/src/main/java/com/test/tnpconnect/MainActivity.kt
@@ -3,6 +3,8 @@ package com.test.tnpconnect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        val bottomView=findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        replaceWithFragement(Home());
+
+        bottomView.setOnItemSelectedListener{
+            when(it.itemId) {
+                R.id.home-> replaceWithFragement(Home())
+                R.id.profile-> replaceWithFragement(Profile())
+                else->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceWithFragement(fragment : Fragment) {
+
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction=fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.FrameLayout,fragment)
+        fragmentTransaction.commit()
     }
 }
+
